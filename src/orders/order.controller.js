3a879373303da4a24ffec6c020e38f73f4f73b67
Hung Nguyen <hunglong750@gmail.com>
2026-06-09 16:03:09 +0700

    remove 404 in order controller

--- src/orders/order.controller.js
+++ src/orders/order.controller.js
@@ -14,9 +14,6 @@ const getOrderByEmail = async (req, res) => {
     try {
         const {email} = req.params;
         const orders = await Order.find({email}).sort({createdAt: -1})
-        if(!orders || orders.length === 0) {
-            return res.status(404).json({message:"Order not found"})
-        }
         res.status(200).json(orders)
     } catch (error) {
         res.status(500).json({message:"Fail to fetch order"})
